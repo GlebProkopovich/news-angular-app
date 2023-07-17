c770858280892b6ba4915ef69601c1aa2f4784a2
Gleb Prokopovich <g.prokopovich@rambler.ru>
2023-07-17 10:22:27 +0400

    changed API-KEY, added padding to news-container, toggle style flex to main

--- src/app/components/cabinet/components/tariffs/tariffs.component.ts
+++ src/app/components/cabinet/components/tariffs/tariffs.component.ts
@@ -15,6 +15,11 @@ export class TariffsComponent {
 
   ngOnInit(): void {
     this.tariffs = tariffs;
+    this.renderer.setStyle(document.querySelector('.main'), 'display', 'block');
+  }
+
+  ngOnDestroy(): void {
+    this.renderer.setStyle(document.querySelector('.main'), 'display', 'flex');
   }
 
   ngAfterViewInit(): void {
